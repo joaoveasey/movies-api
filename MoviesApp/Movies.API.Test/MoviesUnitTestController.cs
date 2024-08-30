efcbe460f051fa36b8a26fb812f94784f00f8882
joaoveasey <jovepedro@gmail.com>
2024-08-30 12:54:31 -0300

    test: creating and configuring the test project with xUnit

---
+++ MoviesApp/Movies.API.Test/MoviesUnitTestController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using movies_api.DTOs.Mappings;
+using movies_api.Infra;
+using movies_api.Interfaces;
+using movies_api.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.API.Test;
+
+public class MoviesUnitTestController
+{
+    public IUnitOfWork repository;
+    public IMapper mapper;
+    public static DbContextOptions<ApplicationDbContext> dbContextOptions {  get; }
+
+    public static string connectionString =
+        "Server=127.0.0.1;Port= 3306;User ID=root;Password=jvp_04022005;Database=movie_db";
+
+    static MoviesUnitTestController()
+    {
+        dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+            .Options;
+    }
+
+    public MoviesUnitTestController()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(new MovieDTOMappingProfile());
+        });
+
+        mapper = config.CreateMapper();
+        var context = new ApplicationDbContext(dbContextOptions);
+        repository = new UnitOfWork(context);
+    }
+}
---
+++ MoviesApp/Movies.API.Test/UnitTests/GetMoviesUnitTest.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using movies_api.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.API.Test.UnitTests;
+
+public class GetMoviesUnitTest : IClassFixture<MoviesUnitTestController>
+{
+    private readonly MovieController _controller;
+
+    public GetMoviesUnitTest(MoviesUnitTestController controller)
+    {
+        _controller = new MovieController(controller.repository, controller.mapper);
+    }
+
+    [Fact]
+    public async Task GetMovieById_OkResult()
+    {
+        // arrange
+
+        var prodId = 2;
+
+        // act
+
+        var data = await _controller.GetMovieById(prodId);
+
+        // assert (fluent assertions)
+
+        data.Result.Should().BeOfType<OkObjectResult>()
+                   .Which.StatusCode.Should().Be(200);
+    }
+
+    [Fact]
+    public async Task GetMovieById_Return_NotFound()
+    {
+        var prodId = 99999;
+
+        var data = await _controller.GetMovieById(prodId);
+
+        data.Result.Should().BeOfType<NotFoundObjectResult>()
+                   .Which.StatusCode.Should().Be(404);
+    }
+
+    [Fact]
+    public async Task GetMovieById_Return_BadRequest()
+    {
+        var prodId = -1;
+
+        var data = await _controller.GetMovieById(prodId);
+
+        data.Result.Should().BeOfType<BadRequestObjectResult>()
+                   .Which.StatusCode.Should().Be(400);
+    }
+}
